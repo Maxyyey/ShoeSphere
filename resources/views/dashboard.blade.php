1413462a99d81ee975a95c53720b034db6def201
Jon Angelo T. Abello <150583210+Xskfy@users.noreply.github.com>
2024-05-30 02:55:57 +0800

    test-ing
    touch

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ mix('/public/css/dashboard.css') }}">
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <div class="v113_3027">
+            <div class="v131_461"></div>
+            <div class="v113_3149"></div>
+            <div class="v113_3150"></div>
+            <span class="v113_3164">40M</span>
+            <div class="name"></div>
+            <span class="v113_3167">30M</span>
+            <div class="name"></div>
+            <span class="v113_3170">20M</span>
+            <div class="name"></div>
+            <span class="v113_3173">10M</span>
+            <div class="name"></div>
+            <span class="v113_3176">5M</span>
+            <div class="name"></div>
+            <span class="v113_3179">0</span>
+            <div class="name"></div>
+            <span class="v113_3202">JAN</span>
+            <span class="v113_3203">FEB</span>
+            <span class="v113_3204">MAR</span>
+            <span class="v113_3205">APRIL</span>
+            <span class="v113_3206">MAY</span>
+            <span class="v113_3207">JUNE</span>
+            <div class="v113_3181">
+                <div class="v113_3182">
+                    <div class="v113_3183">
+                        <div class="v113_3184">
+
+                        </div>
+                        <div class="v113_3185">
+                        </div>
+                    </div>
+                    <div class="v113_3186">
+                        <div class="v113_3187"></div>
+                        <div class="v113_3188"></div></div>
+                        <div class="v113_3189">
+                            <div class="v113_3190"></div>
+                            <div class="v113_3191"></div></div>
+                            <div class="v113_3192">
+                                <div class="v113_3193"></div>
+                                <div class="v113_3194"></div></div>
+                                <div class="v113_3195">
+                                    <div class="v113_3196"></div>
+                                    <div class="v113_3197"></div></div>
+                                    <div class="v113_3198">
+                                        <div class="v113_3199"></div>
+                                        <div class="v113_3200"></div></div></div></div>
+                                        <div class="v113_3238"></div>
+                                        <span class="v113_3151">Finances</span>
+                                        <div class="v113_3152">
+                                            <div class="v113_3153">
+                                                <div class="v113_3154"></div>
+                                                <span class="v113_3155">Expense</span></div>
+                                                <div class="v113_3156">
+                                                    <div class="v113_3157"></div>
+                                                    <span class="v113_3158">Income</span></div></div>
+                                                    <div class="v113_3242"></div>
+                                                    <span class="v113_3244">No of review stars</span>
+                                                    <div class="v113_3245">
+                                                        <div class="v113_3246">
+                                                            <span class="v113_3247">3.5/5 Stars</span>
+                                                            <div class="v113_3248">
+                                                                <div class="v113_3249">
+                                                                    <div class="v113_3250">
+                                                                        <span class="v113_3251">-1.5%</span>
+                                                                        <div class="name">
+
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <span class="v113_3253">Compared to (4.5 last quarter)</span>
+                                                    </div>
+                                                    <div class="v113_3255"></div>
+                                                    <span class="v113_3257">Number of  engagement</span>
+                                                    <div class="v113_3258">
+                                                        <div class="v113_3259">
+                                                            <span class="v113_3260">Avg. of 231 </span>
+                                                            <div class="v113_3261">
+                                                                <div class="v113_3262">
+                                                                    <div class="v113_3263">
+                                                                        <span class="v113_3264">-1.5%</span>
+                                                                        <div class="name"></div></div></div></div></div>
+                                                                        <span class="v113_3266">Compared to (263 last quarter)</span></div>
+                                                                        
+                                                                        <div class="v113_3268"></div>
+                                                                        <span class="v113_3270">Total sales </span>
+                                                                        <div class="v113_3271"><div class="v113_3272">
+                                                                            <span class="v113_3273">$110</span>
+                                                                            <div class="v113_3274">
+                                                                                <div class="v113_3275"><span class="v113_3276">-1.5%</span>
+                                                                                <div class="name"></div></div></div></div>
+                                                                                <span class="v113_3278">Compared to (120 last quarter)</span>
+                                                                            </div>
+                                                                                <span class="v113_3279">Dashboard</span>
+                                                                                <div class="v131_457"></div>
+                                                                                <div class="v131_468">
+                                                                                    <span class="v131_469">SHOESPHERE</span>
+                                                                                </div>
+                                                                                    <div class="v131_465">
+                                                                                        <div class="v131_466"></div>
+                                                                                        <span class="v131_467">Dashboard</span></div>
+                                                                                        <div class="v131_462">
+                                                                                            <div class="v131_463"></div>
+                                                                                            <span class="v131_464">Cart</span>
+                                                                                        </div>
+                                                                                        <div class="v131_458">
+                                                                                            <div class="v131_459">
+                                                                                            </div>
+                                                                                            <span class="v131_460">Shop</span>
+                                                                                        </div>
+                                                                                            <div class="v131_470">
+                                                                                                <div class="v131_471">
+                                                                                                </div>
+                                                                                                <span class="v131_472">Log Out</span>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </body>
+                                                                                    </html>
